d8df1e8e1d7a51dbe63740da8c15cbb567001426
Anna Lewis <annalewis@Annas-MacBook-Air.local>
2016-06-07 16:34:30 -0600

    full functionality, but could be refactored

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ function getRandomIntInclusive(min, max) {
 }
 
 $(document).ready(function(){
-  $("#trackSelect").click(function(event){
-    event.preventDefault()
-    $.get("https://lit-fortress-6467.herokuapp.com/object", function(data, status){
-      console.log(data);
-      var photo1 = data.results[getRandomIntInclusive(0,4)].cover_art;
-      var photo2 = data.results[getRandomIntInclusive(0,4)].cover_art;
-      var photo3 = data.results[getRandomIntInclusive(0,4)].cover_art;
+  $.get("https://lit-fortress-6467.herokuapp.com/object", function(data, status){
+    console.log(data);
+    var photo1 = data.results[getRandomIntInclusive(0,4)].cover_art;
+    var photo2 = data.results[getRandomIntInclusive(0,4)].cover_art;
+    var photo3 = data.results[getRandomIntInclusive(0,4)].cover_art;
 
-      var album1 = document.getElementById("album1");
-      var album2 = document.getElementById("album2");
-      var album3 = document.getElementById("album3");
-      var img1 = document.createElement("img");
-      var img2 = document.createElement("img");
-      var img3 = document.createElement("img");
+    var album1 = document.getElementById("album1");
+    var album2 = document.getElementById("album2");
+    var album3 = document.getElementById("album3");
+    var img1 = document.createElement("img");
+    var img2 = document.createElement("img");
+    var img3 = document.createElement("img");
 
 
-      img1.src = "images/"+photo1;
-      img2.src = "images/"+photo2;
-      img3.src = "images/"+photo3;
+    img1.src = "images/"+photo1;
+    img2.src = "images/"+photo2;
+    img3.src = "images/"+photo3;
 
-      $(album1).append(img1);
-      $(album2).append(img2);
-      $(album3).append(img3);
-    })
+    $(album1).append(img1);
+    $(album2).append(img2);
+    $(album3).append(img3);
+  })
+  $("#trackSelect").click(function(){
+    
   })
 })
 
